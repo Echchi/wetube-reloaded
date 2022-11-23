852becaa89dcc1605311dade48096d4fbde5d696
Echchi <112493260+Echchi@users.noreply.github.com>
2022-11-23 22:25:21 +0900

    Templates(PUG)

---
+++ REANDME.md
@@ -0,0 +1,16 @@
+# Wetube Reloaded
+
+/ => Home
+/join => Join
+/Login => Login
+/search => Search
+
+/users/:id/ => See User
+/users/logout => Log Out
+/users/:id/edit => Edit My Profile
+/user/delete => Delete My Profile
+
+/video/:id => See video
+/video/:id/edit => Edit video
+/video/:id/delete => Delete video
+/video/upload => Upload video
--- src/server.js
+++ src/server.js
@@ -1,20 +1,22 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
+
+console.log(process.cwd())
 
 const app = express();
 const logger = morgan("dev");
 
-const home = (req,res)=> res.send("HELLO");
-const login = (req,res)=> {
-    res.send("login"); 
-}
-
+app.set("view engine","pug");
+app.set("views",process.cwd() + "/src/views");
 app.use(logger);
-app.get("/login",login);
-app.get('/', home);
-
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
     console.log(`🚀 Server listening on port http://localhost:${PORT} 🚀`);
